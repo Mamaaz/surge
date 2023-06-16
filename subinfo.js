1e0c9a20958a1cf09544b30ac720faea266bc797
biu <104364996+biubiumaxxx@users.noreply.github.com>
2023-06-17 00:17:42 +0800

    Update subinfo.js

--- subinfo.js
+++ subinfo.js
@@ -16,14 +16,14 @@ let args = getArgs();
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let content = [`用量：${bytesToSize(used)} ♾️ ${bytesToSize(total)} #怎么才用这么一点点`];
+  let content = [`用量：${bytesToSize(used)} ♾️ ${bytesToSize(total)}`];
 
   if (resetDayLeft) {
-    content.push(`重置：剩余${resetDayLeft}天 #没几天啦`);
+    content.push(`重置：剩余${resetDayLeft}天`);
   }
   if (expire && expire !== "false") {
     if (/^[\d.]+$/.test(expire)) expire *= 1000;
-    content.push(`到期：${formatTime(expire)}，#注意到期时间哦`);
+    content.push(`到期：${formatTime(expire)}`);
   }
 
   let now = new Date();
