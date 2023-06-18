5141b1ba6db55515532f27911e742ee72d998f49
biu <104364996+Mamaaz@users.noreply.github.com>
2023-06-18 12:54:35 +0800

    Update nexitally.js

--- sub/nexitally.js
+++ sub/nexitally.js
@@ -35,8 +35,7 @@ let args = getArgs();
   $done({
     title: `${args.title} | ${hour}:${minutes}`,
     content: content.join("\n"),
-    icon: args.icon || "airplane.circle",
-    "icon-color": args.color || "#007aff",
+   
   });
 })();
 
